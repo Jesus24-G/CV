8a9fd393114c531b962ce7f0c4fcf89e92ddf669
jes <jesgb18@gmail.com>
2025-01-17 00:06:20 -0600

    Agregue pequenas animaciones

---
+++ build/js/app.js
@@ -0,0 +1,36 @@
+
+// var typed2 = new Typed('#contacto', {
+//     strings: ['<i>Whatsapp</i>', '<strong>Gmail</strong>', 'Contacto'],
+//     typeSpeed: 80,
+//     backSpeed: 25,
+//     showCursor: false,
+//     fadeOut: true,
+//     loop: false 
+//   });
+
+  var typed2 = new Typed('#texto-perfil', {
+    strings: ['<i>Perfil Profesional</i>'],
+    typeSpeed: 80,
+    backSpeed: 25,
+    showCursor: false,
+    fadeOut: true,
+    loop: false 
+  });
+
+  var typed2 = new Typed('#texto-experiencia', {
+    strings: ['<i>Experiencia Profesional</i>'],
+    typeSpeed: 80,
+    backSpeed: 25,
+    showCursor: false,
+    fadeOut: true,
+    loop: false 
+  });
+  var typed2 = new Typed('#texto-formacion', {
+    strings: ['<i>Formacion Academica</i>'],
+    typeSpeed: 80,
+    backSpeed: 25,
+    showCursor: false,
+    fadeOut: true,
+    loop: false 
+  });
+    
